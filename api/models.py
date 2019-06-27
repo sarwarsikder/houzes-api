a4645e93ed13098ce9b8a0fcb679a241ced739b6
Iftekhar <iftekhar@workspaceit.com>
2019-06-27 19:02:25 +0600

    Houzes Models, Serializers and APIs creation

--- api/models.py
+++ api/models.py
@@ -1,3 +1,131 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    email_address = models.CharField(max_length=255, null=True)
+    phone_number = models.CharField(max_length=255, null=True)
+    password = models.CharField(max_length=255, null=True)
+    invited_by = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user'
+
+
+class UserLocation(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    lattitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    is_driving = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user_location'
+
+
+class UserVerifications(models.Model):
+    code = models.TextField(null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    expires_at = models.DateTimeField(auto_now_add=True)
+    is_used = models.BooleanField(default=False)
+    verification_type = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user_verifications'
+
+
+class PropertyTags(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True)
+    color = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'property_tags'
+
+
+class PropertyNotes(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    property_id = models.IntegerField(null=True)  # models.ForeignKey(, unique=True, on_delete=models.CASCADE)
+    notes = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'property_notes'
+
+
+class PropertyPhotos(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    property_id = models.IntegerField(null=True)  # models.ForeignKey(, unique=True, on_delete=models.CASCADE)
+    photo_url = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'property_photos'
+
+
+class UserList(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    leads_count = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user_list'
+
+
+class ListProperties(models.Model):
+    list_id = models.ForeignKey(UserList, on_delete=models.CASCADE)
+    property_address = models.CharField(max_length=255, null=True)
+    cad_acct = models.CharField(max_length=255, null=True)
+    gma_tag = models.IntegerField(null=True)
+    lattitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    tag_id = models.ForeignKey(PropertyTags, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'list_properties'
+
+
+class UserDriver(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    list_id = models.ForeignKey(UserList, on_delete=models.CASCADE)
+    distance = models.DecimalField(max_digits=9, decimal_places=6)
+    travel_shape = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user_drives'
+
+
+class UserOwnershipUsage(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    property_id = models.IntegerField(null=True)  # models.ForeignKey(, unique=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'user_ownership_usage'
+
+
+class VisitedProperties(models.Model):
+    drive_id = models.ForeignKey(UserDriver, on_delete=models.CASCADE)
+    property_id = models.IntegerField(null=True)  # models.ForeignKey(, unique=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'visited_properties'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.serializers import *
+from api.models import *
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserLocationViewSet(viewsets.ModelViewSet):
+    queryset = UserLocation.objects.all()
+    serializer_class = UserLocationSerializer
+
+
+class UserVerificationsViewSet(viewsets.ModelViewSet):
+    queryset = UserVerifications.objects.all()
+    serializer_class = UserVerificationsSerializer
+
+
+class PropertyTagsViewSet(viewsets.ModelViewSet):
+    queryset = PropertyTags.objects.all()
+    serializer_class = PropertyTagsSerializer
+
+
+class PropertyNotesViewSet(viewsets.ModelViewSet):
+    queryset = PropertyNotes.objects.all()
+    serializer_class = PropertyNotesSerializer
+
+
+class PropertyPhotosViewSet(viewsets.ModelViewSet):
+    queryset = PropertyPhotos.objects.all()
+    serializer_class = PropertyPhotosSerializer
+
+
+class UserListViewSet(viewsets.ModelViewSet):
+    queryset = UserList.objects.all()
+    serializer_class = UserListSerializer
+
+
+class ListPropertiesViewSet(viewsets.ModelViewSet):
+    queryset = ListProperties.objects.all()
+    serializer_class = ListPropertiesSerializer
+
+
+class UserDriverViewSet(viewsets.ModelViewSet):
+    queryset = UserDriver.objects.all()
+    serializer_class = UserDriverSerializer
+
+
+class UserOwnershipUsageViewSet(viewsets.ModelViewSet):
+    queryset = UserOwnershipUsage.objects.all()
+    serializer_class = UserOwnershipUsageSerializer
+
+
+class VisitedPropertiesViewSet(viewsets.ModelViewSet):
+    queryset = VisitedProperties.objects.all()
+    serializer_class = VisitedPropertiesSerializer
