c388a2204a475f8cfb183ae57478adedea080cbd
Iftekhar <iftekhar@workspaceit.com>
2019-06-28 18:37:43 +0600

    Oauth Implementation session remove

--- houzes_api/settings.py
+++ houzes_api/settings.py
@@ -81,7 +81,6 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
