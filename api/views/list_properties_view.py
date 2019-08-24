3d16f109c81c4930f819c5d7b3b023002ff1591f
Md Mahmud Hasan <sunno.sakib@gmail.com>
2019-08-24 07:36:31 +0000

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !5

--- api/views/list_properties_view.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from api.serializers import *
-from api.models import *
-
-class ListPropertiesViewSet(viewsets.ModelViewSet):
-    queryset = ListProperties.objects.all()
-    serializer_class = ListPropertiesSerializer
-
--- api/views/visited_properties_view.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from api.serializers import *
-from api.models import *
-
-class VisitedPropertiesViewSet(viewsets.ModelViewSet):
-    queryset = VisitedProperties.objects.all()
-    serializer_class = VisitedPropertiesSerializer
