e802f8cdb0f523a2be003c448e24b8b340ea3609
= <=>
2019-09-03 15:23:42 +0600

    lat lon fixed again

--- api/models.py
+++ api/models.py
@@ -163,8 +163,8 @@ class ListProperties(models.Model):
     property_address = models.CharField(max_length=255, null=True)
     cad_acct = models.CharField(max_length=255, null=True)
     gma_tag = models.IntegerField(null=True)
-    latitude = models.DecimalField(max_digits=50, decimal_places=10)
-    longitude = models.DecimalField(max_digits=50, decimal_places=10)
+    latitude = models.DecimalField(max_digits=20, decimal_places=16)
+    longitude = models.DecimalField(max_digits=20, decimal_places=16)
     tag = models.ForeignKey(PropertyTags, on_delete=models.CASCADE, null=True)
     property = models.ForeignKey(Property, on_delete=models.CASCADE)
     # A property can have multiple owner
