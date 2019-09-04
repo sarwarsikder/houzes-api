b91e33d65d5ab5d6c1fb65691437a2d3dd903349
= <=>
2019-09-04 10:17:00 +0600

    fixed mutable problem in user partial update

--- api/views/user_view.py
+++ api/views/user_view.py
@@ -56,8 +56,9 @@ class UserViewSet(viewsets.ModelViewSet):
     # def user_photo_upload(self, request):
 
     def partial_update(self, request, *args, **kwargs):
-        state = request.data._mutable
-        request.data._mutable = True
+        if '_mutable' in request.data:
+            state = request.data._mutable
+            request.data._mutable = True
 
         print(kwargs['pk'])
         if 'password' in request.data:
@@ -74,6 +75,6 @@ class UserViewSet(viewsets.ModelViewSet):
             s3_url = "https://s3.{}.amazonaws.com/{}/{}".format(settings.AWS_REGION, settings.S3_BUCKET_NAME, file_path)
             file_upload(file, file_path)
             request.data['photo'] = s3_url
-
-        request.data._mutable = state
+        if '_mutable' in request.data:
+            request.data._mutable = state
         return super().partial_update(request, *args, **kwargs)
