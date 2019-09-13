da52526ae64c095b9f24885bb4f013d27fa660ba
= <=>
2019-09-13 18:47:12 +0600

    list_properties bulk create

--- api/views/list_properties_view.py
+++ api/views/list_properties_view.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 from rest_framework import viewsets
 from api.serializers import *
 from api.models import *
@@ -5,6 +7,4 @@ from api.models import *
 class ListPropertiesViewSet(viewsets.ModelViewSet):
     queryset = ListProperties.objects.all()
     serializer_class = ListPropertiesSerializer
-    ordering = ['-id']
-
-
+    filterset_fields = ["property_address"]
--- api/views/property_view.py
+++ api/views/property_view.py
@@ -1,15 +1,17 @@
 import json
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from api.serializers import *
 from api.models import *
+
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+    filterset_fields = ["property_address"]
 
-    @action(detail=False,url_path='info/(?P<pk>[\w-]+)')
+    @action(detail=False, url_path='info/(?P<pk>[\w-]+)')
     def get_details_by_google_place_id(self, request, *args, **kwargs):
         property_id = kwargs['pk']
         property = Property.objects.get(id=property_id)
@@ -34,3 +36,31 @@ class PropertyViewSet(viewsets.ModelViewSet):
 
         else:
             return super().create(request, *args, **kwargs)
+
+    @action(detail=False, methods=['post'], url_path='bulk-create')
+    def property_bulk_create(self, request, *args, **kwargs):
+        requestData = request.data
+
+        objs = []
+        iterator=0
+        while iterator< len(requestData['propertyAddress']):
+            property = Property()
+            property.owner_info = json.loads('[{"ownerName" : "'+requestData['ownerName'][iterator]+'","ownerAddress" : "'+requestData['ownerAddress'][iterator]+'"}]')
+            property.property_address = requestData['propertyAddress'][iterator]
+
+            property_create = Property(owner_info= property.owner_info,property_address=property.property_address)
+            property_create.save()
+
+            list_property = ListProperties()
+            list_property.user_list_id = requestData['list']
+            list_property.property_id = property_create.id
+            list_property.property_address = requestData['propertyAddress'][iterator]
+            list_property.owner_info = json.loads('[{"ownerName" : "'+requestData['ownerName'][iterator]+'","ownerAddress" : "'+requestData['ownerAddress'][iterator]+'"}]')
+
+            objs.append(list_property)
+
+            iterator+=1
+
+        ListProperties.objects.bulk_create(objs, batch_size=50)
+
+        return JsonResponse({'response': 'success'})
