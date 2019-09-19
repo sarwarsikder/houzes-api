2d601a94252f7bc2d1f2addd85495cbe768d8fb3
= <=>
2019-09-19 18:35:31 +0600

    user list search field done

--- api/views/user_list_view.py
+++ api/views/user_list_view.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets,filters
 from api.serializers import *
 from api.models import *
 
@@ -7,7 +8,11 @@ from api.models import *
 class UserListViewSet(viewsets.ModelViewSet):
     queryset = UserList.objects.all()
     serializer_class = UserListSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    filterset_fields = "__all__"
+    search_fields = ['name']
     ordering = ['-id']
+
 
     def get_queryset(self):
         return UserList.objects.filter(user_id=self.request.user.id)
