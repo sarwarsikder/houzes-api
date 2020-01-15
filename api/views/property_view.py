95cbb39eb74f9c3e8d7c98dfcfec797453aeab63
Rajesh Saha <rajesh@workspaceit.com>
2020-01-15 15:08:21 +0600

    property lat/lng fetch

--- api/views/property_view.py
+++ api/views/property_view.py
@@ -1,4 +1,5 @@
 import json
+import threading
 import traceback
 import datetime
 
@@ -1108,6 +1109,44 @@ class PropertyViewSet(viewsets.ModelViewSet):
             return Response({'status' : True, 'data': PropertySerializer(property).data, 'message' : 'Successfully deleted tag'})
         except:
             return Response({'status' : False, 'data': {}, 'message' : 'Error deleting tag'})
+
+    @action(detail=False, methods=['GET'], url_path='fetch-missing-lat-lng/(?P<pk>[\w-]+)')
+    def fetch_lat_lng(self, request, *args, **kwargs):
+        try:
+            list_id = kwargs['pk']
+            # update_property_lat_lng(list_id)
+            threading.Thread(target=PropertyViewSet.update_property_lat_lng, args=(list_id,)).start()
+            return Response({'code': 200, 'message': 'Updating Lat/Lng...'})
+        except:
+            traceback.print_exc()
+            return Response({'code': 500, 'message': 'Server Error!'})
+
+    def get_property_info_by_address(address):
+        url = 'https://www.mapdevelopers.com/data.php?operation=geocode'
+        data = {'address': address,
+                'region': 'USA'}
+        res = requests.post(url, data=data)
+        return res.json()
+
+    def update_property_lat_lng(list_id):
+        print("Started...list_id: " + str(list_id))
+        property_list = list(Property.objects.filter(latitude__isnull=True, user_list_id=list_id))
+        for property_info in property_list:
+            try:
+                address = property_info.street + ' ' + property_info.city + ' ' + property_info.state + ' ' + property_info.zip
+                print(address)
+                fetched_data = PropertyViewSet.get_property_info_by_address(address)
+                if 'response' in fetched_data and fetched_data['response']:
+                    lat = float(fetched_data['data']['lat'])
+                    lng = float(fetched_data['data']['lng'])
+                    print(lat, lng)
+                    property_info.latitude = lat
+                    property_info.longitude = lng
+                    Property.objects.filter(id=property_info.id).update(latitude=lat, longitude=lng)
+            except:
+                traceback.print_exc()
+                return Response({'code': 500, 'message': 'Server Error!'})
+        print("Completed...list_id: " + str(list_id))
 
 def update_property_power_trace_info(info_list):
     for info in info_list:
