cadb3c59b87da4139a18be1fc7587e446492e4d9
Mahmud <sunno.sakib@gmail.com>
2020-02-03 12:57:18 +0600

    mailer wizard update

--- api/views/mail_wizard_user_info_view.py
+++ api/views/mail_wizard_user_info_view.py
@@ -1,4 +1,11 @@
+import traceback
+
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from api.serializers import MailWizardUserInfoSerializer
 from api.models import MailWizardUserInfo
 
@@ -6,3 +13,18 @@ from api.models import MailWizardUserInfo
 class MailWizardUserInfoViewSet(viewsets.ModelViewSet):
     queryset = MailWizardUserInfo.objects.all()
     serializer_class = MailWizardUserInfoSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ["user"]
+
+    @action(detail=False, methods=['get'], url_path='get')
+    def get_user_info(self, request):
+        response = {'status': False, 'data': {}, 'message': ''}
+        try:
+            response['data'] = MailWizardUserInfoSerializer(MailWizardUserInfo.objects.filter
+                                                            (user_id=request.user.id).first()).data
+        except MailWizardUserInfo.DoesNotExist:
+            traceback.print_exc()
+        return Response(response)
+
+
+
