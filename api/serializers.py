2a2740a51d82fa1b89d91a3109a6ba33efa310e0
Ehtesham Ahmad Nadim <30365961+EANadim@users.noreply.github.com>
2020-04-08 11:25:05 +0600

    apple log

--- api/serializers.py
+++ api/serializers.py
@@ -60,7 +60,7 @@ class UserSerializer(serializers.ModelSerializer):
             'photo_thumb' : instance.photo_thumb,
             'is_active' : instance.is_active,
             'is_admin' : instance.is_team_admin,
-            'thinker' : True,
+            'thinker' : False,
             'upgrade_info' : {
                 'is_team_invitable' : is_team_invitable,
                 'amount' : float(amount),
